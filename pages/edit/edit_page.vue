7e630a75fb41bcc65dd0a363773c66242b1f3481
qianquan <qian_qian_@126.com>
2023-08-04 18:03:48 +0800

    a little code

--- pages/edit/edit_page.vue
+++ pages/edit/edit_page.vue
@@ -1,25 +1,45 @@
 <template>
 	<view class="content">
-		<view class="preview">
-			<image class="photo" :src="imagePath"></image>
+		<view class="preview" id="saveview">
+			<image class="photo w335" :src="imagePath"></image>
 			<view class="exif_border">
 				<view class="model_info">
-					<view class="model_name">{{exif.Model}}</view>
-					<view class="model_time">{{exif.DateTime}}</view>
+					<view class="model_name">{{ inputModel ? inputModel : exif.Model }}</view>
+					<view class="model_time">{{exif.DateTimeOriginal}}</view>
 				</view>
 				<view class="logo_info">
 					<image :style="{width: brandData.width}" :src="brandData.logoSrc"></image>
 					<view class="line"></view>
 					<view class="exit_info">
-						<view class="photo_exif">{{`${exif.FocalLengthIn35mmFilm}mm  f/${exif.FNumber.numerator / exif.FNumber.denominator} ${ExposureTimeStr} ISO${exif.ISOSpeedRatings}`}}</view>
+						<view class="photo_exif">{{`${focalNum}mm  f/${fNumber} ${ExposureTimeStr} ISO${exif.ISOSpeedRatings}`}}</view>
 						<view class="photo_location">49.11N 116.89W</view>
 					</view>
 				</view>
 			</view>
 		</view>
+		<view class="operation_area w335">
+			<view class="brand_choose ">
+				<view class="uni-list-cell-left brand_input_name">
+					品牌
+				</view>
+				<view class="uni-list-cell-db">
+					<picker @change="bindPickerChange" :value="brandIndex" :range="brandsNameList">
+						<view class="uni-input brand_input_name ml30">
+						{{ inputMake || exif.Make || "未知" }}
+						</view>
+					</picker>
+				</view>
+			</view>
+			<view class="brand_choose">
+				<view class="uni-list-cell-left brand_input_name">型号</view>
+				<input class="uni-input brand_input_name ml30" placeholder="型号" @input="onKeyInput" maxlength="10" v-model="inputModel" />
+			</view>
+			
+		</view>
+		
 		<button class="save_button" @click="onclickSave()">保存照片</button>
-		<text class="info">{{exif.Make}}</text>
 		<button class="save_button" @click="gowebView()">H5页面</button>
+		<canvas id="myCanvas" style="display: none;"></canvas>
 	</view>
 </template>
 
@@ -35,7 +55,10 @@
 				exif: {
 					Make: '', // 品牌
 					Model: '', //型号
-					FocalLengthIn35mmFilm: '', //焦距
+					FocalLength: {
+						denominator: 1, //倍数吧
+						numerator: 0 //焦距值
+					},
 					FNumber: { //光圈
 						denominator: 1, //倍数吧
 						numerator: 0 //光圈值
@@ -46,12 +69,20 @@
 					},
 					ISOSpeedRatings: 100, //ISO
 					DateTime: '',
+					DateTimeOriginal: '',
+					DateTimeDigitized: '',
 				},
 				ExposureTimeStr: '', //快门时间
 				brandData: {
 					logoSrc: '',
 					width: '50px'
-				}
+				},
+				brandIndex: 0,
+				brandsNameList: ['未知','Apple','SONY','Canon','Nikon','Hasu','Fujifilm','Pentax','Olympus','Zeiss'],
+				inputMake: '',
+				inputModel: '',
+				focalNum: '',
+				fNumber: '',
 			}
 		},
 		onLoad() { 
@@ -68,10 +99,20 @@
 					} else {
 						timeStr = `${this.exif.ExposureTime.numerator}/${this.exif.ExposureTime.denominator}s`;
 					}
+
+					this.focalNum = (this.exif.FocalLength.numerator / this.exif.FocalLength.denominator).toFixed(1).replace(/.0/g, "");
+					this.fNumber = (this.exif.FNumber.numerator / this.exif.FNumber.denominator).toFixed(2).replace(/.00/g, "");
+					
 					this.ExposureTimeStr = timeStr;
-					this.brandData = brands[this.exif.Make];
-					console.log(brands);
-					console.log("logo===>", this.brandData );
+					this.inputModel = this.exif.Model || '';
+					if (this.exif.Make) {
+						this.brandData = brands[this.exif.Make] || {};
+					} else {
+						uni.showToast({
+				            title: '未检测到相机数据, 请手动选择。',
+				            icon: 'none'
+				        });
+					}
 					
 				}).catch(e=>{
 				    console.log(e)
@@ -80,21 +121,47 @@
 		},
 		methods: {
 			onclickSave() {
-				uni.saveImageToPhotosAlbum({
-				        filePath: this.imagePath,
-				        success(res) {
-				          uni.showToast({
-				            title: '保存成功',
-				            icon: 'success'
+				
+					// 获取<canvas>标签
+				    const canvas = uni.createCanvasContext('myCanvas', this);
+					const query = uni.createSelectorQuery().in(this);
+					
+				      // 获取<view>标签的内容
+				      query.select('#saveview')
+				        .fields({ node: true, size: true })
+				        .exec((res) => {
+							console.log("save====>",res);
+				          const content = res[0];
+						  if (!content) {
+						                console.log('获取<view>内容失败');
+						                return;
+						 }
+				          const width = content.width;
+				          const height = content.height;
+				
+				          // 绘制<view>的内容到<canvas>
+				          canvas.drawImage(content.node, 0, 0, width, height, 0, 0, width, height);
+				          canvas.draw(false, () => {
+				            // 将<canvas>转换为图片并保存到相册
+				            uni.canvasToTempFilePath({
+				              canvasId: 'myCanvas',
+				              success: (res) => {
+				                uni.saveImageToPhotosAlbum({
+				                  filePath: res.tempFilePath,
+				                  success: () => {
+				                    uni.showToast({ title: '图片保存成功', icon: 'success' });
+				                  },
+				                  fail: () => {
+				                    uni.showToast({ title: '图片保存失败', icon: 'none' });
+				                  },
+				                });
+				              },
+				              fail: () => {
+				                uni.showToast({ title: '转换图片失败', icon: 'none' });
+				              },
+				            }, this);
 				          });
-				        },
-				        fail(err) {
-				          uni.showToast({
-				            title: '保存失败',
-				            icon: 'none'
-				          });
-				        }
-				});
+				        });
 			},
 			gowebView() {
 				uni.navigateTo({
@@ -103,13 +170,30 @@
 						
 					}
 				})
-			}
+			},
+			bindPickerChange: function(e) {
+			    console.log('picker发送选择改变，携带值为', e.detail.value);
+			    this.brandIndex = e.detail.value;
+				const brandName = this.brandsNameList[this.brandIndex] || '';
+				this.inputMake = brandName;
+				this.brandData = brands[brandName] || {};
+			},
+			
+			onKeyInput: function(event) {
+			    this.inputModel = event.target.value
+			},
+			
 		}
 	}
 </script>
 
 <style>
-
+	.w335{
+		width: 335px;
+	}
+	.ml30 {
+		margin-left: 30px;
+	}
 	page {
 		background-color: black;
 		display: flex;
@@ -131,12 +215,11 @@
 		margin-top: 40px;
 		margin-left: 20px;
 		margin-right: 20px;
+		
 		flex-direction: column;
 	}
 	
 	.photo {
-		margin: 0px;
-		padding: 0px;
 		background-color: darkgrey;
 	}
 	
@@ -189,12 +272,34 @@
 	}
 	
 	.photo_exif {
-		font-size: 12px;
+		font-size: 10px;
 		color: #8F8F8F;
 	}
 	.photo_location {
 		font-size: 6px;
 		color: #8F8F8F;
+	}
+	
+	.operation_area {
+		margin-top: 20px;
+		display: flex;
+		/* justify-content: space-between; */
+	}
+	
+	.brand_choose {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		width: 50%;
+	}
+	.brand_input {
+		display: flex;
+		color: white;
+	}
+	.brand_input_name {
+		display: flex;
+		color: white;
+		white-space: nowrap;
 	}
 	
 	.save_button {
